62dfafb846adcf971957fe3570ee00533e3b8358
mrbaummath <baum1286@gmail.com>
2022-11-17 17:31:59 -0500

    postgresql db

--- ff7/settings.py
+++ ff7/settings.py
@@ -78,8 +78,8 @@ WSGI_APPLICATION = 'ff7.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ff7-django-API',
     }
 }
 
